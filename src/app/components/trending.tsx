8192fd39e834f7afc834fe611dde4245ab32b70e
Syed Konain Nasir <konain611@gmail.com>
2025-02-11 22:37:52 +0500

    Fixed Image Issue

--- src/app/components/trending.tsx
+++ src/app/components/trending.tsx
@@ -1,20 +1,25 @@
+import Image from 'next/image';
+import image01 from '../images/tp1.png';
+import image02 from '../images/tp2.png';
+import image03 from '../images/tp3.png';
+import image04 from '../images/LP5.png';
 
-export default function Trending(){
-    return( 
-        <div>
-        
+export default function Trending() {
+  return (
+    <div>
+
       <h1 className="text-3xl font-bold text-[#1A0B5B] mb-8 mt-[70px] text-center">
         Trending Products
       </h1>
       <div className="mb-6 flex items-center justify-center space-x-11">
 
         <div className="featured-card group hover:border-pink-500">
-          <img
+          <Image
             alt="White cantilever chair"
             className="w-full h-32 object-cover mb-4"
             height={150}
-            src="https://s3-alpha-sig.figma.com/img/6eac/9ed6/f0567dbeace7c9dbaa02b7b75570f3bc?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ShVtA8s5iMjpp1sml5nxcqBFOpIzBbKAJ8jaIOpRs9DlTNoPt0BXSMHJGOYcD9GJuxcQMfK65AZQbt3iRLKEPWZno5T2nOtoVdknkpq~6so-NYD9K9d4DSAlhMqsRX7~FD0cY0LVnIV4AZGVNYjMy96ZhPu6J7LdCcO0ABt5U6K1s-qJr4vKes7Lk0BzEmxnvEL6QXYVyDhXfxSv2E0-zsv3W53ISLmOZKrDjZW1bwXhvly9FlDBEUJVL7rQdy-ZFi7GQraClo5gDVt2B7FyNQZ0~GOcgat7STo-jfgbFRPrAofscNPDGjPxf2PfBxigr1cYwXY64zoCB27KbmjvyQ__"
             width={150}
+            src={image01}
           />
           <div className="text-center">
             <h2 className="text-[#151875] font-bold font-sans text-[18px]">Cantilever chair</h2>
@@ -22,12 +27,12 @@ export default function Trending(){
           </div>
         </div>
         <div className="featured-card group hover:border-pink-500">
-          <img
+          <Image
             alt="White cantilever chair"
             className="w-full h-32 object-cover mb-4"
             height={150}
-            src="https://s3-alpha-sig.figma.com/img/e70a/c286/dc012e6f46348c947e76cbc241026273?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=fnOHUQ0VV8DVt3d~FA8fRU6c~-RoPUfQ38j-qN2zDvCrNPmkRfSqgY1oCiJQaTZlUTKxRDcnVhxqotRy2jjfTL4Yv1dA1zZe51gp~hwriVZv-wAYdvChmY1iCBhirfVSKP0bAbj9kEI97nHDnC8P03VCLURpIOfy~F-yslIhC2GqYtbK86A4oUqHr-Bknz9gz9xwrm1IwjxVOc8AcbAjhAUO9GN6ByAju-Gw~jMtr6WZog8-9ZOKKR4eOER~hskJrZWfptlMWCITdYMzkRfJSt3PonsMn4EnvxhZ3E1KlBFvyL2aFjv~aA5oo2qVzQFfj6EZ5tmV6uGHgIQTVldcvA__"
             width={150}
+            src={image02}
           />
           <div className="text-center">
             <h2 className="text-[#151875] font-bold font-sans text-[18px]">Cantilever chair</h2>
@@ -35,12 +40,12 @@ export default function Trending(){
           </div>
         </div>
         <div className="featured-card group hover:border-pink-500">
-          <img
+          <Image
             alt="White cantilever chair"
             className="w-full h-32 object-cover mb-4"
             height={150}
-            src="https://s3-alpha-sig.figma.com/img/402f/024e/43e865f7def49808ba73dcffe65b6b21?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cidKxqVbYY1t4oCkY~rCS1xFCZtR-rYpzEP8rX6oruR3gPPOr1wpaX72z2YdCIhjetZlv5IOYuQAccStZ1bLDtbXmLVcpl1cA2yJxHSct-wR6Vp59cCw5K-OJPZ9C5bahthofVLMuDBo8lTN4UiW7l6oEkk~0Nx2LJ60QXxAtenMXPrkj-Usl9V3~1xmKnjEXqXUHq2ZYWyBaQt0eWhv3gjZYw3HtOlwzdoOaNauSAhKNCY84fNmxGsJpFDQr4Ni~Obx2F0sHGtJYLWUSubsyKfPYsnhNOPrHRrtkEtPT~F5UriVwmkPI7b4p3vzMR7jFu3HK-JiW9q7d5guQCvXow__"
             width={150}
+            src={image03}
           />
           <div className="text-center">
             <h2 className="text-[#151875] font-bold font-sans text-[18px]">Cantilever chair</h2>
@@ -48,120 +53,120 @@ export default function Trending(){
           </div>
         </div>
         <div className="featured-card group hover:border-pink-500">
-          <img
+          <Image
             alt="White cantilever chair"
             className="w-full h-32 object-cover mb-4"
             height={150}
-            src="https://s3-alpha-sig.figma.com/img/0081/87bd/b43127cc352f32aef1be3bab70906d81?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ZIGKfdtLzEgHMKhw~aqegJR7L5YcGgudDkH2jD6Uj87CeYNgVaT8sQnwULBZ3v4wHkVOtlIoV2N3-toZyYJRReFGaesEIKfnoQJKO7FKa7w2Ucj3AwQsY2Krg6rgeIGRrImSmK4dXdRLUZ1yjnhPCAPLGztdzuk8WrhP6m1GsE6hvCPpyBqoh6dBK1YPoDDvFydAgjIbiQjvGda1NZO3SCk3FtIUjxSfZY0PB7F2jQ4ifNVpwSLm8mcs1VSveB62xkIDpP1uzZ7pfCCmcsZn9qI2GxooLriK27X4C30AS~E~JfSp8vToma8xQdWL~MWP14NfPjq5xnX0YcdaEL0V-w__"
             width={150}
+            src={image04}
           />
           <div className="text-center">
             <h2 className="text-[#151875] font-bold font-sans text-[18px]">Cantilever chair</h2>
             <p className="text-gray-700 font-sans text-[14px]">$26.00 <span className="line-through text-gray-500">$42.00</span></p>
           </div>
         </div>
-    
+
       </div>
 
 
 
-  <div className="container mx-auto p-4">
-    <div className="flex flex-wrap justify-center items-center gap-6">
+      <div className="container mx-auto p-4">
+        <div className="flex flex-wrap justify-center items-center gap-6">
 
-      <div className="featured-card4">
-        <div className="bg-pink-100 p-2 rounded-lg">
-          <h2 className=" font-bold text-blue-900 mb-2">
-            23% off in all products
-          </h2>
-          <a className="text-pink-600 font-semibold" href="#">
-           <u> Shop Now</u>
-          </a>
-          <div className="mt-1">
-            <img
-              alt="Black clock"
-              className="w-full h-auto"
-              height={30}
-              src="https://s3-alpha-sig.figma.com/img/429c/934a/dc8a953600bab29da0ff9265ab06bb80?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=SYd0ScXWe6P4nNWxHPGbpB3UAmlUUEiJNlADm4wK3mPiSTItKWMI3J2td6BhrPaQJCfZm9OFks1gU3dJRJryMvi~PDtiemiD0RXNddhDdgYWaQ6k6URHJXs2GbtsSqAF7Qk2crtihraQ6nKr~HSTvlZXWXIya6BSJNUKBfP~u0AuCWk48dycEbyeyakVb4uLUAwtQ0eBgdEk3zgxNIIV1DxbffkaQdmJxbqAFdYncZaihat7aJArulKzh6WZHMmXVFwjyIaDqOIoCi3r3AjV0YNPubbdmxanDcRcYqupZTdLfKPBswuaRDTzVfgwQXvmn3I~i5e90zFyT2z2v2XpCA__"
-              width={30}
-            />
+          <div className="featured-card4">
+            <div className="bg-pink-100 p-2 rounded-lg">
+              <h2 className=" font-bold text-blue-900 mb-2">
+                23% off in all products
+              </h2>
+              <a className="text-pink-600 font-semibold" href="#">
+                <u> Shop Now</u>
+              </a>
+              <div className="mt-1">
+                <img
+                  alt="Black clock"
+                  className="w-full h-auto"
+                  height={30}
+                  src="/images/clock1.png"
+                  width={30}
+                />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-  
-      <div className="featured-card4">
-        <div className="bg-blue-100 p-2 rounded-lg">
-          <h2 className=" font-bold text-blue-900 mb-2">
-            23% off in all products
-          </h2>
-          <a className="text-pink-600 font-semibold" href="#">
-           <u>View Collection</u>
-          </a>
-          <div className="mt-1">
-            <img
-              alt="Black clock"
-              className="w-full h-auto"
-              height={20}
-              src="https://s3-alpha-sig.figma.com/img/5c7c/ebfc/bebf2952988250989ce526d7c72d8017?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=HkpOr9sCLuhGzR6x9OtsIyDWp3Kg0cIm-EcdrK92HlYKyKhGo10YerXJuwzhCvJr65FffQbA8oR5VAlv8uNKnq-guEgy046HXXqHYxZiBi3iEPGUHkTG~Dni6mSPEiBaVamyrL82~4l4P2IABXtfaWWKq4rqMoYxqnRoCA-WKiUt5-3KMEgMHYUkKWE9AnRHUm-7Yjrt56gpTqAwlqzmq-Y0ofaVqMbAuI05QnhtmVajEe4FTIxMSGdj978TdyXxFNABW8OsfuM9wGYh8vwZZu15TJOn4wIilG1kiz9pNPmWAdcLyt97udiYNEjuV6ZrVtIRVN195fyHA-aRAJgqjw__"
-              width={20}
-            />
-          </div>
-        </div>
-      </div>
-    
 
-    <div className="featured-card4 mt-8">
-      <div className="flex flex-col space-y-4">
-        <div className="flex items-center">
-          <img
-            alt="Executive Seat chair"
-            className="w-16 h-16"
-            height={50}
-            src="https://storage.googleapis.com/a1aa/image/aGjvTxZKQQp0GRZiXAWnLVHdneLIe9fP4gvTb38JDMGlp2xnA.jpg"
-            width={50}
-          />
-          <div className="ml-4">
-            <h3 className="text-blue-900 font-semibold">
-              Executive Seat chair
-            </h3>
-            <p className="text-gray-600">$32.00</p>
+          <div className="featured-card4">
+            <div className="bg-blue-100 p-2 rounded-lg">
+              <h2 className=" font-bold text-blue-900 mb-2">
+                23% off in all products
+              </h2>
+              <a className="text-pink-600 font-semibold" href="#">
+                <u>View Collection</u>
+              </a>
+              <div className="mt-1">
+                <img
+                  alt="Black clock"
+                  className="w-full h-auto"
+                  height={20}
+                  src="/images/clock2.png"
+                  width={20}
+                />
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="flex items-center">
-          <img
-            alt="Executive Seat chair"
-            className="w-16 h-16"
-            height={50}
-            src="https://storage.googleapis.com/a1aa/image/aGjvTxZKQQp0GRZiXAWnLVHdneLIe9fP4gvTb38JDMGlp2xnA.jpg"
-            width={50}
-          />
-          <div className="ml-4">
-            <h3 className="text-blue-900 font-semibold">
-              Executive Seat chair
-            </h3>
-            <p className="text-gray-600">$32.00</p>
-          </div>
-        </div>
-        <div className="flex items-center">
-          <img
-            alt="Executive Seat chair"
-            className="w-16 h-16"
-            height={50}
-            src="https://storage.googleapis.com/a1aa/image/aGjvTxZKQQp0GRZiXAWnLVHdneLIe9fP4gvTb38JDMGlp2xnA.jpg"
-            width={50}
-          />
-          <div className="ml-4">
-            <h3 className="text-blue-900 font-semibold">
-              Executive Seat chair
-            </h3>
-            <p className="text-gray-600">$32.00</p>
-          </div>
+
+
+          <div className="featured-card4 mt-8">
+            <div className="flex flex-col space-y-4">
+              <div className="flex items-center">
+                <img
+                  alt="Executive Seat chair"
+                  className="w-16 h-16"
+                  height={50}
+                  src="/images/chair1.png"
+                  width={50}
+                />
+                <div className="ml-4">
+                  <h3 className="text-blue-900 font-semibold">
+                    Executive Seat chair
+                  </h3>
+                  <p className="text-gray-600">$32.00</p>
+                </div>
+              </div>
+              <div className="flex items-center">
+                <img
+                  alt="Executive Seat chair"
+                  className="w-16 h-16"
+                  height={50}
+                  src="/images/chair1.png"
+                  width={50}
+                />
+                <div className="ml-4">
+                  <h3 className="text-blue-900 font-semibold">
+                    Executive Seat chair
+                  </h3>
+                  <p className="text-gray-600">$32.00</p>
+                </div>
+              </div>
+              <div className="flex items-center">
+                <img
+                  alt="Executive Seat chair"
+                  className="w-16 h-16"
+                  height={50}
+                  src="/images/chair1.png"
+                  width={50}
+                />
+                <div className="ml-4">
+                  <h3 className="text-blue-900 font-semibold">
+                    Executive Seat chair
+                  </h3>
+                  <p className="text-gray-600">$32.00</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+
+
     </div>
-  </div>
-
-
-        </div>
-      )
+  )
 }
