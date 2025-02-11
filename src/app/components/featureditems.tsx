5a9e677a4e76b9a59b066cf5b7783d37b173e662
Syed Konain Nasir <konain611@gmail.com>
2025-02-11 19:48:55 +0500

    Fixed images issue

--- src/app/components/featureditems.tsx
+++ src/app/components/featureditems.tsx
@@ -1,3 +1,10 @@
+import Image from 'next/image';
+import chairImage1 from '../images/FP1.png';
+import chairImage2 from '../images/FP2.png';
+import chairImage3 from '../images/FP3.png';
+import chairImage4 from '../images/FP4.png';  
+
+
 export default function Featured() {
   return (
     <>
@@ -7,13 +14,13 @@ export default function Featured() {
       <div className="flex items-center justify-center space-x-11">
 
         <div className="featured-card group hover:border-blue-500">
-          <img
-            alt="White cantilever chair"
-            className="w-full h-32 object-cover mb-4"
-            height={150}
-            src="https://s3-alpha-sig.figma.com/img/1ade/62f0/6db0a7630b347e6423288fc09447f1a8?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=A1ASbX5PQ2xgkZQUVT7fty3DynXlxANL8qO7IW9wlpV268-RxQDtE0hx8rHqWqchih~We1J7ct41jlqizDklTOyDwJf5rNeexRN0TQ4ofReelqg8AAuSqkLUmt5CRUl0gMtBipAaF6qgJHFfXKsXhYeLJ3xkLNjy3YHciF5OtkD~ion-eT4JSywJ14Llo~WekPvS9oB6Vcrrf66ItGWDKcZ0Q36qjQDhv96sBH~gPG~xVzO8N7RWSWdqDTVd-k6DvfIpRZGhEIM-Xq4SzNW0Zg85v9CcuqGx~kqQtYx9qngixxXg3moXFnASwQurY8uP7m3xjVf7nYkMx-KrVER0oA__"
-            width={150}
-          />
+            <Image
+              alt="White cantilever chair"
+              className="w-full h-32 object-cover mb-4"
+              height={150}
+              width={150}
+              src={chairImage1}
+            />
           <div className="text-center">
             <h2 className="text-pink-500 font-bold font-sans text-[18px]">Cantilever chair</h2>
             <p className="text-gray-500 font-sans text-[14px]">Code - Y523201</p>
@@ -22,11 +29,11 @@ export default function Featured() {
         </div>
     
         <div className="featured-card group hover:border-green-500">
-          <img
+          <Image
             alt="Beige cantilever chair"
             className="w-full h-32 object-cover mb-4"
             height={150}
-            src="https://s3-alpha-sig.figma.com/img/db5f/e91e/81f40d5069977639c120b46b02efd85d?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=GboJek~DN13nVfBwMPMzJD0e05J9~EjxRxU97-8tABYs-uGN2qEB0ez~QWWItTQHzb0yUmFm04d9yvSbauLegwnMgwwPQIDBOHM3EBEqQnGA2lO5XCCTnILUZnykm3uWWsiQONR8OZXKKJwDIRh7Z~XEvKn9e1V0IqY0MDJFldpyPVD-gsAGJlGTu9phA05u7S~cMFunQ~FPaf8ppJu9JB-7qYhxRjPaqR3t~DSHzhjL7iwyPMw4PMjHW8D6sTDhO5fhsGW87K7AO1TwOtQyUbUpiDwIdco6AqqTEZvRCiZOigp4qhSfNYlUU8RdhUwjjjCaw09UK9To~ITvjJDvXw__"
+            src={chairImage2}
             width={150}
           />
           <div className="text-center">
@@ -37,11 +44,11 @@ export default function Featured() {
         </div>
    
         <div className="featured-card group hover:border-red-500">
-          <img
+          <Image
             alt="Blue cantilever chair"
             className="w-full h-32 object-cover mb-4"
             height={150}
-            src="https://s3-alpha-sig.figma.com/img/ac53/427c/ec2feb66129667973939b2ea5191c695?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=kUi~zB9-c~eGQGzT8TwiRHmFUgjb56OaKZd08UFUtANf-9dGje10O1RlD16wWiH1CvV2vf~ZetN~-TEO2f8ayS856NvZYuR36C0XKy39f1oD8NAAVXMmWl2KmM9tvUQpTIDKPtRBZnhW5y92B-GLHOCOB~Jw9L9epJ~qAFSPm2lcvR2CznAYRcQvOreGoBQfmf2LyOhyK3CZHmkGA8xxhBGy-qnkyaxgHI5Vr4odi6x4Yo610KD5is-C9E1m3FXbXdaDyX~H0Lf1Ia0arsV5NrYRa1j42BKoG7NHpD1ZUYUqxuZg3E15gTDNZ855pGsDBMNU4M6GLIoq7lu9WTCBwA__"
+            src={chairImage3}
             width={150}
           />
           <div className="text-center">
@@ -52,11 +59,11 @@ export default function Featured() {
         </div>
      
         <div className="featured-card group hover:border-purple-500">
-          <img
+          <Image
             alt="Gray cantilever chair"
             className="w-full h-32 object-cover mb-4"
             height={150}
-            src="https://s3-alpha-sig.figma.com/img/fe50/2c91/678776be52471704ed6aa26cd1d28dd8?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=QiOcOmW4pI3zdbRIy~FsfCDeiAEPJ~ABdrN-HK-u-3vVZjkNsB61LDvNkR9SUVAtxQ3xPtK63SKl7NoGBW1hta7iHodghSuzkElrrW~gRC-KzqJYFR6kLC0IULdcRtv226jyLdN0Vi75KH1FNi2HHlngDTWVa3ieNZD-qeZBPZ~Kz1wsvbAdtftPfeFZMl7Y19hnuUbENdLCUrh3aeb9fshdvYf9hFceCyYfOor7pxJ25PUEz-xpy2dJM5HQTsd~JMnVf2caw2il9NqCyMIYzEz91OFSICSxK3P9L69F5nmVkStedViXXuvQVzV3ukUCCeg0AApGyh4ftLoDDchkrg__"
+            src={chairImage4}
             width={150}
           />
           <div className="text-center">
--- src/app/components/promotionalpage.tsx
+++ src/app/components/promotionalpage.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image';
+import lampImage from '../images/lamp.png';
+import sofaImage from '../images/pinkSofa.png';
 
 export default function PromoPage() {
   return (
@@ -11,7 +13,7 @@ export default function PromoPage() {
             alt="Hanging lamp"
             height={387}
             width={387}
-            src="https://s3-alpha-sig.figma.com/img/b0ab/7b45/906d2aaf430d5bb6b86d462a9e91a8af?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=kpoOmKWmB6sveVWwYmPqgDk-Z3k-BY4czXh1MaXvmyeNa1Ah-SP3bEZ1sBNGu8PwV~d5HyQ18ppw-6pKmFwXmJW~nTBs9igUOOIYQwcwnxR9SLx9anJXnbBw2IgmYS2jl4i02MbgYUUqlCZ~S6GyxPFsg0Kv8FIDvWGJYXrZIEKZAjgnqrSQ-FjuvIe-Iy05LgFuSik-9eJcVk~DDzaIDNmJWPm6JlXUnbyix1Gbz~YvmgLQMLOZrA27KzdO4C0OMUZEGNGRS62-keSSbHg5g4LCjOHrFtCqOgHcEQ242MmMjyZ~GT0Ng9OPIfdIebK5B~DbB35DABbaqagXQI2nbQ__"
+            src={lampImage}
           />
         </div>
 
@@ -39,8 +41,8 @@ export default function PromoPage() {
             height={629}
             width={629}
             className="rounded-lg"
-            src="https://s3-alpha-sig.figma.com/img/c453/494a/61ecc67b127029400b09a70cf2e9e973?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=bfnjCHRCyGpKFG0MkP9C3u1Ni9VT4laLaRoxphGre-qXfr7F3B6WPRT92guukA~T0SFy2O-s842jF1sEgs4OEahM~aXDtFYsk7sRWbyByMgLcvK3SaZgSwBoR4mvcopt2HGaJi7vrUWqLhHsgI-b~I870glBwplx~jAaC-7gbZFbUk5nTKj2ktd-UY5bynhH9Kaw1aN3ZdRFad2FBdvOAh4vK9q35EWInU4T8dEtGAMEzi2hcVmvFM41uSUspuBvAQ1o9gOw7RW0VoIhA0qlC5kqoAkq5bz0ZS9Q-ZQVTDjrgAMbVZI2nyDBlw229-V3M4lSDVoI8D25XpKejTci9g__"
-          />
+            src={sofaImage}
+/>
           <div className="absolute top-4 right-4 bg-blue-500 text-white text-lg px-4 py-2 rounded-full font-bold">
             50% off
           </div>
